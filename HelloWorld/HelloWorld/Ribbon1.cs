c8fc767a145dfd828dc8bf854f5b8f757847b636
Özgür TEKİNGÖZ (Müşteri ve CRM 1) <otekingoz@fintek.local>
2021-09-22 22:48:16 +0300

    hello world added

---
+++ HelloWorld/HelloWorld/Ribbon1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using Excel= Microsoft.Office.Interop.Excel;
+namespace HelloWorld
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnHelloWorld_Click(object sender, RibbonControlEventArgs e)
+        {
+            Worksheet currentSheet = Globals.ThisAddIn.GetActiveWorkSheet();
+            currentSheet.Range["A1"].Value = "Hello World";
+            currentSheet.Columns.AutoFit();
+        }
+    }
+}
