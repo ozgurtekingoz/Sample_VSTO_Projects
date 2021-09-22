e04e97f42ecad0c1dfcba3c1543c90218d406f9d
Özgür TEKİNGÖZ (Müşteri ve CRM 1) <otekingoz@fintek.local>
2021-09-22 22:59:23 +0300

    dropdown project added

---
+++ DropDown/DropDown/Ribbon1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace DropDown
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            RibbonDropDownItem item;
+            using (var reader = new StreamReader(@"...\test.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    item = Factory.CreateRibbonDropDownItem();
+                    var line = reader.ReadLine();
+                    item.Label = line;
+                    dropDown1.Items.Add(item);
+                }
+            }
+        }
+    }
+}
